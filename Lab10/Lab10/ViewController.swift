f89b3c612df0bf28d17e4e67c5098b0f6bc131fd
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-05-27 11:28:38 +0300

    create project

---
+++ Lab10/Lab10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab10
+//
+//  Created by Yulia Raitsyna on 27.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
